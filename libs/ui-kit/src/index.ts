9b2b206b639c08e36efc667a93a7a6e813f0a7cd
Subesh-Dev <developer.sub@gmail.com>
2026-05-27 16:32:17 +0530

    feat(platform): integrate VisualQueryBuilder into UQL bar

--- libs/ui-kit/src/index.ts
+++ libs/ui-kit/src/index.ts
@@ -1,3 +1,3 @@
 export { ThemeProvider, useTheme, ThemeToggle } from './theme'
-export { Button, Card, Input, Spinner, Badge, Layout, Table, CodeBlock, TraceWaterfall } from './components'
+export { Button, Card, Input, Spinner, Badge, Layout, Table, CodeBlock, TraceWaterfall, VisualQueryBuilder } from './components'
 export { VERSION } from './version'
